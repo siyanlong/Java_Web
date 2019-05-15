24bc78520a8b80702274ac7be26930765725d1cd
siyanlong <44536879+siyanlong@users.noreply.github.com>
2019-05-15 16:02:57 +0800

    Spring Boot学习

--- SpringBoot/spring-boot-web-restful-crud/src/main/java/com/cit/siyl/config/MyMvcConfig.java
+++ SpringBoot/spring-boot-web-restful-crud/src/main/java/com/cit/siyl/config/MyMvcConfig.java
@@ -3,9 +3,11 @@ package com.cit.siyl.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.cit.siyl.component.LoginHandlerInterceptor;
 import com.cit.siyl.component.MyLocalResolver;
 
 //使用WebMvcConfigurer可以来扩展SpringMVC的功能
@@ -13,12 +15,23 @@ import com.cit.siyl.component.MyLocalResolver;
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
+	//添加视图映射
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("login");
 		registry.addViewController("/index.html").setViewName("login");
+		registry.addViewController("/main.html").setViewName("dashboard");
 	}
-	
+		
+	//注册拦截器
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//静态资源：*.css，*.js
+		//Spring Boot已经做好了静态资源映射，因此不需要排除对静态资源的过滤，但需要排序登陆界面的拦截，要不然连登陆界面都访问不了了
+		registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+			.excludePathPatterns("/index.html", "/", "/user/login");
+	}
+
 	@Bean//将组件注册在容器
 	public LocaleResolver localeResolver() {
 		return new MyLocalResolver();
--- SpringBoot/spring-boot-web-restful-crud/src/main/java/com/cit/siyl/controller/LoginController.java
+++ SpringBoot/spring-boot-web-restful-crud/src/main/java/com/cit/siyl/controller/LoginController.java
@@ -10,23 +10,22 @@ import java.util.Map;
 @Controller
 public class LoginController {
 
-	//@DeleteMapping
-	//@PutMapping
-	//@GetMapping
-    //@RequestMapping(value = "/user/login",method = RequestMethod.POST)
-    @PostMapping(value = "/user/login")//@PostMapping可以替代@RequestMapping(method = RequestMethod.POST)
-    public String login(@RequestParam("username") String username,
-                        @RequestParam("password") String password,
-                        Map<String,Object> map, HttpSession session){
-        if(!StringUtils.isEmpty(username) && "123456".equals(password)){
-            //登陆成功，防止表单重复提交，可以重定向到主页
-            session.setAttribute("loginUser",username);
-            return "redirect:/main.html";
-        }else{
-            //登陆失败
-            map.put("msg","用户名密码错误");
-            return  "login";
-        }
+	// @DeleteMapping
+	// @PutMapping
+	// @GetMapping
+	// @RequestMapping(value = "/user/login",method = RequestMethod.POST)
+	@PostMapping(value = "/user/login") // @PostMapping可以替代@RequestMapping(method = RequestMethod.POST)
+	public String login(@RequestParam("username") String username, @RequestParam("password") String password,
+			Map<String, Object> map, HttpSession session) {
+		if (!StringUtils.isEmpty(username) && "123456".equals(password)) {
+			// 登陆成功，防止表单重复提交，可以重定向到主页
+			session.setAttribute("loginUser", username);
+			return "redirect:/main.html";
+		} else {
+			// 登陆失败
+			map.put("msg", "用户名密码错误！");
+			return "login";
+		}
 
-    }
+	}
 }
