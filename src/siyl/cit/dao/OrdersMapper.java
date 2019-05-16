d1aebacc88ddc510e92c18099825517913d0724e
siyanlong <44536879+siyanlong@users.noreply.github.com>
2019-05-16 15:47:37 +0800

    Mybatis自动生成代码

---
+++ src/siyl/cit/dao/OrdersMapper.java
@@ -0,0 +1,17 @@
+package siyl.cit.dao;
+
+import siyl.cit.model.Orders;
+
+public interface OrdersMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
---
+++ src/siyl/cit/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package siyl.cit.dao;
+
+import siyl.cit.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
