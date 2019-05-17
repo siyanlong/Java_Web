2a1a9b21c4bc610b4b31178121d200b09e10972c
siyanlong <44536879+siyanlong@users.noreply.github.com>
2019-05-17 17:04:18 +0800

    Spring Boot学习

---
+++ SpringBoot/spring-boot-web-jsp/src/main/java/com/cit/siyl/SpringBootWebJspApplication.java
@@ -0,0 +1,13 @@
+package com.cit.siyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebJspApplication {
+
+	public static void main(String[] args) {
+		// 传入SpringBoot应用的主程序
+		SpringApplication.run(SpringBootWebJspApplication.class, args);
+	}
+}
